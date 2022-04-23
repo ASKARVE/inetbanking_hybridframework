353d2bd3c9127bf2ccf4ff5c1e6bb40ae6ca7e52
ASKARVE <104243841+ASKARVE@users.noreply.github.com>
2022-04-23 10:40:27 +0530

    Update Test.java

--- hybridFramework/src/test/java/com/vandvtechsolution/netbanking/pageobjects/Test.java
+++ hybridFramework/src/test/java/com/vandvtechsolution/netbanking/pageobjects/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("main method");
+		Test t = new Test();
+		t.test();
+	}
+	public void test(){
+		System.out.println("test method");
 	}
 }
